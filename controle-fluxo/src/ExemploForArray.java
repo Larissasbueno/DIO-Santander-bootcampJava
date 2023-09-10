ee3e3f0daf9ce7e7267248b19fe26dd5c3379725
Larissasbuenno <larissasbuenno@gmail.com>
2023-09-10 13:06:38 -0300

    controle-fluxo

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,12 @@
+public class ExemploForArray {
+    // ExemploFor.java
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+            //Forma abreviada
+        for(String aluno : alunos) {
+          System.out.println("Nome do aluno é: " + aluno);
+        }
+    
+    }
+    }
+    
